353d8b3f489b7ff07d27dc898b495f875982888a
Zigr1 <36465099+Zigr1@users.noreply.github.com>
2020-11-18 10:02:36 +1030

    Added invalid input handling to api end point

--- api/index.php
+++ api/index.php
@@ -8,17 +8,31 @@
 
 	$result = null;
 
-	// include Assignment class specification
+	// include api client class specification
 	include('client.class.php');
 
-	// instantiate new object of Assignment class with parameters values from request object
-	$assignment = new Client($_REQUEST['dateTimeStart'],
-							 $_REQUEST['timeZoneStart'],
-							 $_REQUEST['dateTimeEnd'],
-							 $_REQUEST['timeZoneEnd'],
-							 $_REQUEST['meassure'],
-							 $_REQUEST['convertTo']
-							);
+	// instantiate new object of the class with parameters values from request object
+	try {
+		$assignment = new Client($_REQUEST['dateTimeStart'],
+					 $_REQUEST['timeZoneStart'],
+					 $_REQUEST['dateTimeEnd'],
+					 $_REQUEST['timeZoneEnd'],
+					 $_REQUEST['meassure'],
+					 $_REQUEST['convertTo']
+					);
+
+	# Handle wrong input
+	} catch(Exception $err) {
+		$errorCode = $err->getCode();
+		$errorMessage = $err->getMessage();
+
+		// set response code - 422 Wrong input
+	    	http_response_code(422);
+
+		// tell the user about wrong input parameter value
+		echo json_encode(array('status' => $errorCode, 'message' => $err->getMessage()));
+		return;
+	}
 
 	// perform appropriate method according to input parameters values
 	$result = $assignment->chooseMethod($method,$route);
