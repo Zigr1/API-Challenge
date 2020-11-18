73dc72513e4ad5fb4b47f6468ad6368c7e1cdd45
Zigr1 <36465099+Zigr1@users.noreply.github.com>
2020-11-19 00:47:48 +1030

    Check of request method in index.php
    Allow sendind request only with GET and POST

--- api/index.php
+++ api/index.php
@@ -2,6 +2,8 @@
 
 	# API access point
 
+	header("Content-Type: application/json; charset=UTF-8");
+
 	// receive request object
 	$route 	= $_SERVER['REQUEST_URI'];
 	$method = $_SERVER['REQUEST_METHOD'];
@@ -34,8 +36,18 @@
 		return;
 	}
 
-	// perform appropriate method according to input parameters values
-	$result = $assignment->chooseMethod($method,$route);
+	// allow only POST or GET to send request
+	if ($method == 'POST' || $method == 'GET') {
+		// perform appropriate method according to input parameters values
+		$result = $assignment->chooseMethod($method,$route);
+	} else {
+		// set response code - 405 Method not allowed
+	    http_response_code(405);
+
+	    // tell the user about wrong input parameter value
+	    echo json_encode(array('status' => 405, 'message' => 'You should use either POST or GET to send request'));
+	    return;		
+	}
 
 	// return response in JSON format
 	echo json_encode($result);
